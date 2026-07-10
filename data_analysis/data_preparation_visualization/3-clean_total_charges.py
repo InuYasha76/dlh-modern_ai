cd94b3587d331382913bdd806ff4e959209b6769
Guillaume CANAVAGGIO <guillaume.canavaggio@ai.dlh.lu>
2026-07-10 14:59:57 +0200

    Fixed task 3, added df.copy()

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -4,7 +4,7 @@ Module to clean missing values in the TotalCharges column.
 """
 
 
-def clean_total_charges(df, method='drop'):
+def clean_total_charges(df, method="drop"):
     """
     Handles missing values in the TotalCharges column using various strategies.
     Args:
@@ -13,15 +13,20 @@ def clean_total_charges(df, method='drop'):
     Returns:
         pandas.DataFrame: The modified DataFrame.
     """
-    match method:
-        case 'drop':
-            df = df.dropna(subset=['TotalCharges'])
-        case 'median':
-            median_TC = df['TotalCharges'].median()
-            df.loc[:, 'TotalCharges'] = df['TotalCharges'].fillna(median_TC)
-        case 'impute':
-            imputation = df['MonthlyCharges'] * df['tenure']
-            df.loc[:, 'TotalCharges'] = df['TotalCharges'].fillna(imputation)
-        case _:
-            raise ValueError(f"Unknown cleaning method: {method}")
-    return df
+    df_copy = df.copy()
+
+    if method == "drop":
+        return df_copy.dropna(subset=["TotalCharges"])
+
+    elif method == "median":
+        median = df_copy["TotalCharges"].median()
+        df_copy.loc[:, "TotalCharges"] = df_copy["TotalCharges"].fillna(median)
+
+    elif method == "impute":
+        imput = df_copy["MonthlyCharges"] * df_copy["tenure"]
+        df_copy.loc[:, "TotalCharges"] = df_copy["TotalCharges"].fillna(imput)
+
+    else:
+        raise ValueError(f"Unknown cleaning method: {method}")
+
+    return df_copy
