7f94b6d1a99cbbe1dcab2fdd35623b64f2fdcb6b
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-07-23 17:46:55 +0200

    fixed line>80

--- machine_learning/linear_models/6-svm.py
+++ machine_learning/linear_models/6-svm.py
@@ -9,7 +9,7 @@ def get_SVM_model(name, random_state):
     Creates and returns an untrained Support Vector Classifier (SVC).
     Arguments:
         name: A string indicating the kernel type ('linear', 'poly', or 'rbf').
-        random_state: An integer used to set the random seed for reproducibility.
+        random_state (int) used to set the random seed for reproducibility.
     Returns:
         model: An untrained instance of sklearn.svm.SVC
     """
