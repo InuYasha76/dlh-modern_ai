7e9434deb4b8f9dbfe3ae087ce9fa18d302642ab
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 22:00:11 +0200

    Update 15-encode_features.py
    Fixed issue

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -31,7 +31,9 @@ def encode_features(df):
     churn_le = preprocessing.LabelEncoder()
     df["Churn"] = churn_le.fit_transform(df["Churn"])
     # Step 2: OrdinalEncoder for binary columns
-    binary_oe = preprocessing.OrdinalEncoder(categories=[['No', 'Yes']])
+    binary_oe = preprocessing.OrdinalEncoder(
+        categories=[['No', 'Yes']] * len(binary_cols)
+    )
     df[binary_cols] = binary_oe.fit_transform(df[binary_cols]).astype(int)
     # Step 3: One-hot encoding for Contract and PaymentMethod
     df = pd.get_dummies(
