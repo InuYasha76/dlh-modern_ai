b0e0553c1460d65cec06cb6224a97bace7cc71ae
InuYasha76 <guillaume.canavaggio@gmail.com>
2026-08-23 22:09:53 +0200

    Update 17-split_data.py
    returning X_train, X_test, y_train, y_test in required order

--- data_analysis/data_preparation_visualization/17-split_data.py
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -21,10 +21,11 @@ def split_data(df, target="Churn", test_size=0.2, random_state=42):
         return 1
     X = df.drop(columns=[target])
     y = df[target]
-    return model_selection.train_test_split(
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X,
         y,
         test_size=test_size,
         random_state=random_state,
         stratify=y,
     )
+    return X_train, y_train, X_test, y_test
